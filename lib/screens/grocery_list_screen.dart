e846b0d64ad8e691638d11d47af9ab04c47a2825
dainavi.purav <dainavi.purav@cmss.in>
2024-04-28 18:10:30 +0530

    navigation added for form

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_006_shopping_list_app/screens/new_item_screen.dart';
 
 import '../widgets/grocery_list.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
+
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  void _addNewItem() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return const NewItemScreen();
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addNewItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: const GroceryList(),
     );
